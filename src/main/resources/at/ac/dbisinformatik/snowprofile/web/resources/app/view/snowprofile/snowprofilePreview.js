5c383cce147b49c8a505299247469fe55b278b8a
aleksandar.stojakovic <aleksandar.stojakovic@b0e66d11-08a8-0410-84da-a809a5ca4a2f>
2012-10-02 17:52:33 +0000

    git-svn-id: https://dbis-informatik.uibk.ac.at/svn/snowprofile/trunk/code@112 b0e66d11-08a8-0410-84da-a809a5ca4a2f

---
+++ src/main/resources/at/ac/dbisinformatik/snowprofile/web/resources/app/view/snowprofile/snowprofilePreview.js
@@ -0,0 +1,102 @@
+Ext.define('LWD.view.snowprofile.snowprofilepreview' ,{
+    extend: 'Ext.grid.Panel',
+    alias : 'widget.snowprofilepreview',
+	itemId: 'snowprofilePreviewGrid',
+    store: 'Snowprofile',
+	
+	border: false,
+	height: '100%',
+
+    tbar: [{
+        text: 'Neues Schichtprofil',
+        iconCls: 'icon-add',
+        handler: function(){
+    		var grid = this.up("grid");
+    		var rowEditing = grid.getPlugin("rowplugin");
+    		grid.getStore().insert(0, new LWD.model.LayerProfile());
+    		rowEditing.startEdit(0, 0);
+        }
+    }, '-', {
+        itemId: 'delete',
+        text: 'Löschen',
+        iconCls: 'icon-delete',
+        handler: function(){
+    		var grid = this.up("grid");
+            var selection = grid.getView().getSelectionModel().getSelection()[0];
+            if (selection) {
+            	grid.getStore().remove(selection);
+            	grid.getStore().fireEvent("dataupdate", grid.getStore());
+            }
+        }
+    }],
+
+    columns: [
+		{
+			header: 'Von Höhe[cm]',
+			dataIndex: 'depthTop_content',
+			flex: 1,
+			editor: {
+			    xtype: 'numberfield',
+	            allowBlank: false,
+	            minValue: 0,
+			}
+		},
+		{
+            header: 'Kornform 1',
+            dataIndex: 'grainFormPrimary',
+            flex: 1,
+            editor: {
+                allowBlank: false
+            }
+		},
+		{
+			header: 'Kornform 2',
+			dataIndex: 'grainFormSecondary',
+			flex: 1,
+			editor: {
+				allowBlank: false
+			}
+		},
+		{
+			header: 'Grösse[D][mm] avg',
+			dataIndex: 'grainSize_Components_avg',
+			flex: 1,
+			editor: {
+				allowBlank: false
+			}
+		},
+		{
+			header: 'Grösse[D][mm] avg max',
+			dataIndex: 'grainSize_Components_avgMax',
+			flex: 1,
+			editor: {
+				allowBlank: false
+			}
+		},
+		{
+			header: 'Härte[K]',
+			dataIndex: 'hardness',
+			flex: 1,
+			editor: {
+				allowBlank: false
+			}
+		},
+		{
+			header: 'Feuchte',
+			dataIndex: 'lwc_content',
+			flex: 1,
+			editor: {
+				allowBlank: false
+			}
+		}
+	],
+	
+    initComponent: function() {
+		this.on('edit', this.commit);
+        this.callParent(arguments);
+    },
+    
+    commit: function(edit, e) {
+    	this.getStore().fireEvent("dataupdate", this.getStore());
+    }
+});
